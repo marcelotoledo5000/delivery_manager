e4e5322e5e24223d820e444f218dcc916950f25f
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-18 14:44:48 -0300

    add validations to Transporter

--- app/models/transporter.rb
+++ app/models/transporter.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class Transporter < ApplicationRecord
+  validates :corporate_name,
+            :brand_name,
+            :domain,
+            :registration_number,
+            :full_address, presence: true
+
+  validates :corporate_name,
+            :brand_name,
+            :domain,
+            :full_address, uniqueness: true
+
+  validates :registration_number, uniqueness: { case_sensitive: false }
+
+  validates :registration_number, format: { with: %r{\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}},
+                                            message: 'deve ter o formato: 00.000.000/0000-00' }
 end
