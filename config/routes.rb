745ce353c3098939faf846f31b63c79403efbb05
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-18 20:13:02 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :transporters, only: %i[show new create]
+  resources :transporters, only: %i[index show new create]
 end
