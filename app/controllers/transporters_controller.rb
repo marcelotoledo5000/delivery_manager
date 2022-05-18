970de76bb5fe85e06510ee5c827f3b01358b5ab2
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-18 14:42:22 -0300

    Update transporters_controller.rb
    create new and create actions

--- app/controllers/transporters_controller.rb
+++ app/controllers/transporters_controller.rb
@@ -4,7 +4,34 @@ class TransportersController < ApplicationController
   before_action :set_transporter, only: %i[show]
 
   def show; end
+
+  def new
+    @transporter = Transporter.new
+  end
+
+  def create
+    @transporter = Transporter.new(transporter_params)
+
+    if @transporter.save
+      redirect_to @transporter, notice: 'Transportadora cadastrada com sucesso!'
+    else
+      flash.now[notice] = 'Transportadora não cadastrada.'
+
+      render :new
+    end
+  end
+
   private
+
+  def transporter_params
+    params.require(:transporter).permit(
+      :corporate_name,
+      :brand_name,
+      :domain,
+      :registration_number,
+      :full_address
+    )
+  end
 
   def set_transporter
     @transporter = Transporter.find(params[:id])
