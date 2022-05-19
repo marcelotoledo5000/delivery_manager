982a9200d5b9d52c75e350cbe72fd7efe645b828
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-18 21:37:14 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts
+puts '-------------------------------------------------'
+puts 'Start "seeds"'
+puts '-------------------------------------------------'
+puts
+
+puts '------------ creating transporters ------------'
+
+FactoryBot.create_list(:transporter, 2)
+
+puts
+puts '-------------------------------------------------'
+puts 'Finished "seeds"'
+puts '-------------------------------------------------'
+puts
